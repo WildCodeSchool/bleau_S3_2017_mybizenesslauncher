845b51154fd0e9a516217f8994796a5e93d020d3
Christophe_vinc <vincentchristophe177@gmail.com>
2017-05-16 17:04:32 +0200

    ajout layout

--- src/MBLBundle/Controller/DefaultController.php
+++ src/MBLBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('MBLBundle:Default:index.html.twig');
+        return $this->render('@MBL/Users/index.html.twig');
     }
 }
