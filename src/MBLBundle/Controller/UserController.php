8bd52ad5cde7c612ad5908a08ef8483975ea1758
CharlyAuzanet <charly.auzanet@gmail.com>
2017-06-14 16:46:22 +0200

    Nouveau profil rechercher, mise en place du css avec l'ajax

--- src/MBLBundle/Controller/UserController.php
+++ src/MBLBundle/Controller/UserController.php
@@ -27,6 +27,7 @@ class UserController extends Controller
 
     public function homepageProfilAction()
     {
+
         return $this->render('@MBL/Users/homepageProfil.html.twig');
 
     }
@@ -111,7 +112,7 @@ class UserController extends Controller
         return $this->render('@MBL/Users/createProjetAddProfil.html.twig',
             array('form_pro' => $form_pro->createView(),
                 'projet' => $projet,
-'profil_Recheche_exist' => $profil_Recheche_exist,
+                'profil_Recheche_exist' => $profil_Recheche_exist,
 
             ));
     }
@@ -130,7 +131,8 @@ class UserController extends Controller
     public function showMyProjectAction()
     {
         $em = $this->getDoctrine()->getManager();
-        $projects = $em->getRepository('MBLBundle:Projet')->findMyProject(40);
+        $id = $this->getUser()->getId();
+        $projects = $em->getRepository('MBLBundle:Projet')->findMyProject($id);
 
         return $this->render('@MBL/Users/showMyProject.html.twig', array(
             'projects' => $projects
